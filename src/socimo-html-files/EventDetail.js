ce42880be12a3f07a0127f44dfcb932923981783
AnhKhoa1405 <60646884+AnhKhoa1405@users.noreply.github.com>
2021-10-30 16:07:33 +0700

    Update EventDetail.js

--- src/socimo-html-files/EventDetail.js
+++ src/socimo-html-files/EventDetail.js
@@ -2,6 +2,7 @@
 import React ,{ useState, useEffect} from "react";
 import { Link ,useLocation } from "react-router-dom";
 import axios from "axios";
+import HeaderPage from "./Header";
 
 function EventDetail()  {
   let location = useLocation();
